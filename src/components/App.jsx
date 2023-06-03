65d5c447421af30473fb8aee310777466a2ea1f5
GlobalSatUA <alex.trenbach@gmail.com>
2023-06-03 17:41:57 +0300

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import Statistics from "./Statistics/statistics";
+import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/friendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +21,18 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+     
     </div>
   );
 };
+
---
+++ src/components/FriendList/friendList.js
@@ -0,0 +1,15 @@
+const FriendList = ({ friends }) => {
+    return (
+        <ul className="friend-list">
+            {friends.map(({ id, avatar, name, isOnline }) => (
+            <li className="item" key={id}>
+                <span className="status">{isOnline}</span>
+                <img className="avatar" src={avatar} alt="User avatar" width="48" />
+                <p className="name">{name}</p>
+            </li>
+              ))}
+        </ul>
+    )
+}
+
+export default FriendList;
